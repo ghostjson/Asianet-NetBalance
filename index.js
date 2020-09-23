2f16ac7b89be777240ff9f6ffc3fd382587b7a27
Akhil Ravindran <akhilravindran3@gmail.com>
2020-09-23 19:25:01 +0530

    verison 0.8.0

---
+++ index.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer");
+const config = require("./config.json");
+
+async function run() {
+  let url = "https://myabb.in/";
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
+
+  await page.type("#DUser", config.username, { delay: config.buffer_delay });
+  await page.type("#Pwd", config.password, { delay: config.buffer_delay });
+
+  await page.click(".btn.waves-effect.waves-light", { delay: 1000 });
+
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  const usage_page = await browser.newPage();
+  await usage_page.goto(`${url}/goToForm?formId=CUP001`, {
+    waitUntil: "networkidle0",
+  });
+
+  const remaining = await getRemainingData(usage_page);
+  const used = await getUsedData(usage_page);
+  const total = await getTotalData(usage_page);
+
+  console.log(`Total: ${toGB(total)}GB`);
+  console.log(`Used: ${toGB(used)}GB`);
+  console.log(`Remaining: ${toGB(remaining)}GB`);
+
+  await browser.close();
+}
+
+async function getRemainingData(page) {
+  return await page.evaluate(() => {
+    return document.querySelector("#totalOctets").innerText;
+  });
+}
+
+async function getUsedData(page) {
+  return await page.evaluate(() => {
+    return document.querySelector("#totalOctet").innerText;
+  });
+}
+
+async function getTotalData(page) {
+  return await page.evaluate(() => {
+    return document.querySelector("#totalOc").innerText;
+  });
+}
+
+function toGB(mb) {
+  return (mb / 1024).toFixed(2);
+}
+
+run();
---
+++ install.bat
@@ -0,0 +1,6 @@
+echo node %cd%\index.js > net-balance.bat
+echo pause >> net-balance.bat
+
+npm install
+
+pause
